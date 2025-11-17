7008deed9f96a2512e5c8e30d065279948e0eaf7
danilokacanski <kacanski.ra26.2020@gmail.com>
2025-11-17 11:44:42 +0100

    add practice_tasks

--- week02_plots/practice_tasks.jl
+++ week02_plots/practice_tasks.jl
@@ -20,28 +20,28 @@ plot(t, y, lw=2, label="y1", xlabel="t", ylabel="y")
 # =========================================
 
 t = 0:0.01:2π
-tp = rem.(t, 2)
+tp = rem.(t, 1)
 sinus = sin.(t)
 
-y = sinus .* (tp .< 1)
+y = sinus .* (tp .< 0.5)
 
 plot(t, sinus, color=:red, ls=:dash, label="sin(t)")
-plot!(t, step, color=:blue, lw=2, label="y(t)", xlabel="t", ylabel="y")
-
+plot!(t, y, color=:blue, lw=2, label="y(t)", xlabel="t", ylabel="y")
 
 # =========================================
 # Zadatak 3
 # Sawtooth + under-limit curve
 # =========================================
 
-t = 0:0.1:10
+t = 0:0.01:10
 tp = rem.(t, 2)
 
-base = tp
-limit = 4 .- abs.(t .- 5)
+y1 = 4 .* (tp .< 1)
+y2 = 0.4 .* t
+y = min.(y1,y2)
 
-plot(t, limit, color=:red, ls=:dash, label="y1")
-plot!(t, base, lw=2, label="y", xlabel="t", ylabel="y")
+plot(t, y1, color=:red, ls=:dash, label="y1")
+plot!(t, y, lw=2, label="y", xlabel="t", ylabel="y")
 
 
 # =========================================
@@ -49,18 +49,12 @@ plot!(t, base, lw=2, label="y", xlabel="t", ylabel="y")
 # Periodic flat-top waveform + scatter(sin)
 # =========================================
 
-t = 0:0.1:9
-tp = rem.(t, 3)
+t = 0:0.5:9
+sinus = 2 * sin.(t * pi / 3)
 
-y = 0 .* (tp .< 1) .+
-    1 .* ((tp .>= 1) .& (tp .< 2)) .+
-    0 .* (tp .>= 2)
+y = max.(min.(sinus, 1), -1)
 
-# sin samples at integer points
-ts = 0:1:9
-ys = sin.(ts)
-
-scatter(ts, ys, color=:blue, label="sin")
+scatter(t, sinus, color=:blue, label="sin")
 plot!(t, y, lw=2, color=:orangered, label="y", xlabel="t", ylabel="y")
 
 
@@ -71,7 +65,7 @@ plot!(t, y, lw=2, color=:orangered, label="y", xlabel="t", ylabel="y")
 
 t = -3:0.01:3
 
-plot(t, t.^2 .+ 1, lw=2, label="x^2 + 1")
+plot(t, t.^2 .- 1, lw=2, label="x^2 - 1")
 plot!(t, -t.^2 .+ 1, lw=2, label="-x^2 + 1", xlabel="t", ylabel="y")
 
 
@@ -80,11 +74,12 @@ plot!(t, -t.^2 .+ 1, lw=2, label="-x^2 + 1", xlabel="t", ylabel="y")
 # Triangular periodic signal with amplitude 3
 # =========================================
 
-t = 0:0.1:15
+t = 0:0.01:15
 
 tp = rem.(t, 3)      # period = 3 sec
-y = 3 .* tp .* (tp .< 1.5) .+
-    3 .* (3 .- tp) .* (tp .>= 1.5)
+y = 3 .* tp .* (tp .< 1) .+
+    3 .* ((tp .>= 1) .& (tp .< 2)) .+
+    3 .* (3 .- tp) .* (tp .>= 2)
 
 plot(t, y, lw=2, label="y1", xlabel="t", ylabel="y")
 
@@ -112,7 +107,8 @@ tp = rem.(t, 2)   # period = 2
 circle = sqrt.(1 .- (tp .- 1).^2)
 
 # Triangle from 0→2 amplitude 2
-triangle = 2 .* abs.(tp .- 1)
+triangle = 2 .* tp .* (tp .< 1) .+
+           2 .* (2 .- tp) .* (tp .>= 1)
 
 # Composite = min(circle, triangle)
 y = min.(circle, triangle)
